d7c4f1ead5009ca1f80081a64007fbd7894c4ac2
Administrator <Administrator@DESKTOP-204HED7>
2019-06-20 21:23:37 +0800

    高俊凯的杰作

---
+++ src/main/java/com/hb/service/XiangQingService.java
@@ -0,0 +1,11 @@
+package com.hb.service;
+
+import java.util.List;
+
+import com.hb.pojo.CaiDanPojo;
+
+public interface XiangQingService {
+
+	CaiDanPojo findxiangqing(Integer caiDanId);
+	
+}
---
+++ src/main/java/com/hb/service/impl/XiangQingServiceImpl.java
@@ -0,0 +1,24 @@
+package com.hb.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hb.mapper.XiangQingMapper;
+import com.hb.pojo.CaiDanPojo;
+import com.hb.service.XiangQingService;
+
+@Service
+public class XiangQingServiceImpl implements XiangQingService{
+
+	@Autowired
+	private XiangQingMapper xiangQingMapper;
+	@Override
+	public CaiDanPojo findxiangqing(Integer caiDanId) {
+		CaiDanPojo paiMing = xiangQingMapper.findxiangqing(caiDanId);
+		System.out.println("哈哈哈呵呵呵呵呵");
+		return paiMing;
+	}
+
+}
