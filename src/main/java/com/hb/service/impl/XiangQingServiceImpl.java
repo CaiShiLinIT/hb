0f31753549df72ec50e941d584b6d82186e8a7ac
Administrator <Administrator@DESKTOP-204HED7>
2019-06-20 20:58:52 +0800

    走你走你

--- src/main/java/com/hb/service/impl/XiangQingServiceImpl.java
+++ src/main/java/com/hb/service/impl/XiangQingServiceImpl.java
@@ -17,7 +17,7 @@ public class XiangQingServiceImpl implements XiangQingService{
 	@Override
 	public CaiDanPojo findxiangqing(Integer caiDanId) {
 		CaiDanPojo paiMing = xiangQingMapper.findxiangqing(caiDanId);
-		System.out.println("哈哈哈");
+		System.out.println("哈哈哈呵呵呵呵呵");
 		return paiMing;
 	}
 
